11ca0e66108b7f65a44a06219e96005d43e43a92
Maira Bello <maira.araujo@liferay.com>
2015-06-02 11:31:37 -0300

    Adds Align utility

---
+++ src/Align.js
@@ -0,0 +1,170 @@
+'use strict';
+
+import Position from './Position';
+
+/**
+ * Align utility. Computes region or best region to align an element with
+ * another. Regions are relative to viewport, make sure to use element with
+ * position fixed, or position absolute when the element first positioned
+ * parent is the body element.
+ */
+class Align {
+
+  /**
+   * Aligns the element with the best region around alignElement. The best
+   * region is defined by clockwise rotation starting from the specified
+   * `position`. The element is always aligned in the middle of alignElement
+   * axis.
+   * @param {!Element} element Element to be aligned.
+   * @param {!Element} alignElement Element to align with.
+   * @param {Align.Top|Align.Right|Align.Bottom|Align.Left} pos
+   *     The initial position to try. Options `Align.Top`, `Align.Right`,
+   *     `Align.Bottom`, `Align.Left`.
+   * @static
+   */
+  static align(element, alignElement, position) {
+    var bestRegion = this.getAlignBestRegion(element, alignElement, position);
+
+    var computedStyle = window.getComputedStyle(element, null);
+    if (computedStyle.getPropertyValue('position') !== 'fixed') {
+      var docEl = window.document.documentElement;
+      bestRegion.top += docEl.clientTop + window.pageYOffset;
+      bestRegion.left += docEl.clientLeft + window.pageXOffset;
+    }
+
+    element.style.top = bestRegion.top + 'px';
+    element.style.left = bestRegion.left + 'px';
+  }
+
+  /**
+   * Returns the best region to align element with alignElement. The best
+   * region is defined by clockwise rotation starting from the specified
+   * `position`. The element is always aligned in the middle of alignElement
+   * axis.
+   * @param {!Element} element Element to be aligned.
+   * @param {!Element} alignElement Element to align with.
+   * @param {Align.Top|Align.Right|Align.Bottom|Align.Left} pos
+   *     The initial position to try. Options `Align.Top`, `Align.Right`,
+   *     `Align.Bottom`, `Align.Left`.
+   * @return {DOMRect} Best region to align element.
+   * @static
+   */
+  static getAlignBestRegion(element, alignElement, position) {
+    var bestArea = 0;
+    var bestPosition = position;
+    var bestRegion = this.getAlignRegion(element, alignElement, bestPosition);
+    var tryPosition = bestPosition;
+    var tryRegion = bestRegion;
+    var viewportRegion = Position.getRegion(window);
+
+    for (var i = 0; i < 4;) {
+      if (Position.intersectRegion(viewportRegion, tryRegion)) {
+        var visibleRegion = Position.intersection(viewportRegion, tryRegion);
+        var area = visibleRegion.width * visibleRegion.height;
+        if (area > bestArea) {
+          bestArea = area;
+          bestRegion = tryRegion;
+          bestPosition = tryPosition;
+        }
+        if (Position.insideViewport(tryRegion)) {
+          break;
+        }
+      }
+      tryPosition = (position + (++i)) % 4;
+      tryRegion = this.getAlignRegion(element, alignElement, tryPosition);
+    }
+
+    return bestRegion;
+  }
+
+  /**
+   * Returns the region to align element with alignElement. The element is
+   * always aligned in the middle of alignElement axis.
+   * @param {!Element} element Element to be aligned.
+   * @param {!Element} alignElement Element to align with.
+   * @param {Align.Top|Align.Right|Align.Bottom|Align.Left} pos
+   *     The position to align. Options `Align.Top`, `Align.Right`,
+   *     `Align.Bottom`, `Align.Left`.
+   * @return {DOMRect} Region to align element.
+   * @static
+   */
+  static getAlignRegion(element, alignElement, position) {
+    var r1 = Position.getRegion(alignElement);
+    var r2 = Position.getRegion(element);
+    var top = 0;
+    var left = 0;
+
+    switch(position) {
+      case Align.Top:
+        top = r1.top - r2.height;
+        left = r1.left + r1.width/2 - r2.width/2;
+        break;
+      case Align.Right:
+        top = r1.top + r1.height/2 - r2.height/2;
+        left = r1.left + r1.width;
+        break;
+      case Align.Bottom:
+        top = r1.bottom;
+        left = r1.left + r1.width/2 - r2.width/2;
+        break;
+      case Align.Left:
+        top = r1.top + r1.height/2 - r2.height/2;
+        left = r1.left - r2.width;
+        break;
+    }
+
+    return {
+      bottom: top + r2.height,
+      height: r2.height,
+      left: left,
+      right: left + r2.width,
+      top: top,
+      width: r2.width
+    };
+  }
+
+  /**
+   * Checks if specified value is a valid position. Options `Align.Top`,
+   *     `Align.Right`, `Align.Bottom`, `Align.Left`.
+   * @param {Align.Top|Align.Right|Align.Bottom|Align.Left} val
+   * @return {Boolean} Returns true if value is a valid position.
+   * @static
+   */
+  static isValidPosition(val) {
+    return 0 <= val && val <= 3;
+  }
+}
+
+/**
+ * Represents the `Align.Top` constant.
+ * @type {Number}
+ * @default 0
+ * @static
+ */
+Align.Top = 0;
+
+/**
+ * Represents the `Align.Right` constant.
+ * @type {Number}
+ * @default 1
+ * @static
+ */
+Align.Right = 1;
+
+/**
+ * Represents the `Align.Bottom` constant.
+ * @type {Number}
+ * @default 2
+ * @static
+ */
+Align.Bottom = 2;
+
+/**
+ * Represents the `Align.Left` constant.
+ * @type {Number}
+ * @default 3
+ * @static
+ */
+Align.Left = 3;
+
+export default Align;
---
+++ test/Align.js
@@ -0,0 +1,124 @@
+'use strict';
+
+import dom from 'bower:metaljs/src/dom/dom';
+import Position from '../src/Position';
+import Align from '../src/Align';
+
+var element;
+var mutable;
+var center;
+
+describe('Align', function() {
+  after(function() {
+    dom.exitDocument(element);
+    dom.exitDocument(mutable);
+    dom.exitDocument(center);
+    window.scrollTo(0, 0);
+  });
+
+  before(function() {
+    dom.enterDocument('<div id="element" style="position:absolute;height: 25px;width:25px;"></div>');
+    dom.enterDocument('<div id="center" style="position:absolute;top:100px;left:100px;width:50px;height:50px;"></div>');
+    dom.enterDocument('<div id="mutable" style="position:absolute;width:50px;height:50px;"></div>');
+    element = dom.toElement('#element');
+    mutable = dom.toElement('#mutable');
+    center = dom.toElement('#center');
+  });
+
+  beforeEach(function() {
+    mutable.style.top = '100px';
+    mutable.style.left = '100px';
+    mutable.style.bottom = '';
+    mutable.style.right = '';
+  });
+
+  it('should align at bottom', function() {
+    Align.align(element, center, Align.Bottom);
+    assert.strictEqual('150px', element.style.top);
+    assert.strictEqual('112.5px', element.style.left);
+  });
+
+  it('should align at right', function() {
+    Align.align(element, center, Align.Right);
+    assert.strictEqual('112.5px', element.style.top);
+    assert.strictEqual('150px', element.style.left);
+  });
+
+  it('should align at top', function() {
+    Align.align(element, center, Align.Top);
+    assert.strictEqual('75px', element.style.top);
+    assert.strictEqual('112.5px', element.style.left);
+  });
+
+  it('should align at left', function() {
+    Align.align(element, center, Align.Left);
+    assert.strictEqual('112.5px', element.style.top);
+    assert.strictEqual('75px', element.style.left);
+  });
+
+  it('should try to align at top then move right', function() {
+    mutable.style.top = 0;
+    Align.align(element, mutable, Align.Top);
+    assert.strictEqual('12.5px', element.style.top);
+    assert.strictEqual('150px', element.style.left);
+  });
+
+  it('should try to align at right then move bottom', function() {
+    mutable.style.left = (Position.getRegion(window).right - 50) + 'px';
+    Align.align(element, mutable, Align.Right);
+    var mutableRegion = Position.getRegion(mutable);
+    var elementRegion = Position.getRegion(element);
+    assert.strictEqual('150px', element.style.top);
+    assert.strictEqual((mutableRegion.left + mutableRegion.width/2 - elementRegion.width/2) + 'px', element.style.left);
+  });
+
+  it('should try to align at bottom then move left', function() {
+    mutable.style.bottom = Position.getRegion(window).bottom + 'px';
+    mutable.style.top = (Position.getRegion(window).bottom - 50) + 'px';
+    Align.align(element, mutable, Align.Bottom);
+    var mutableRegion = Position.getRegion(mutable);
+    var elementRegion = Position.getRegion(element);
+    assert.strictEqual((mutableRegion.top + mutableRegion.height/2 - elementRegion.height/2) + 'px', element.style.top);
+    assert.strictEqual((mutableRegion.left - elementRegion.width) + 'px', element.style.left);
+  });
+
+  it('should try to align at left then move top', function() {
+    mutable.style.top = '100px';
+    mutable.style.left = 0;
+    Align.align(element, mutable, Align.Left);
+    assert.strictEqual('75px', element.style.top);
+    assert.strictEqual('12.5px', element.style.left);
+  });
+
+  it('should compute pageYOffset when aligning element with position absolute', function() {
+    element.style.position = 'absolute';
+    mutable.style.position = 'relative';
+    mutable.style.top = '10000px';
+    mutable.style.left = '10000px';
+    window.scrollTo(5000, 5000);
+    Align.align(element, mutable, Align.Left);
+    assert.strictEqual('10020.5px', element.style.top);
+    assert.strictEqual('9983px', element.style.left);
+  });
+
+  it('should not compute pageYOffset when aligning element with position fixed', function() {
+    element.style.position = 'fixed';
+    mutable.style.position = 'relative';
+    mutable.style.top = '10000px';
+    mutable.style.left = '10000px';
+    window.scrollTo(5000, 5000);
+    Align.align(element, mutable, Align.Left);
+    assert.strictEqual('5020.5px', element.style.top);
+    assert.strictEqual('4983px', element.style.left);
+  });
+
+  it('should check if align position is valid', function() {
+    assert.ok(Align.isValidPosition(Align.Top));
+    assert.ok(Align.isValidPosition(Align.Right));
+    assert.ok(Align.isValidPosition(Align.Bottom));
+    assert.ok(Align.isValidPosition(Align.Left));
+
+    assert.ok(!Align.isValidPosition(-1));
+    assert.ok(!Align.isValidPosition(4));
+  });
+});
