04818f8c2d667c3ebd6f76a315b27df9c0d406c0
Maira Bello <maira.araujo@liferay.com>
2016-02-10 13:52:37 -0300

    Starts using new metal modules

--- test/Position.js
+++ test/Position.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import dom from 'metal/src/dom/dom';
+import dom from 'metal-dom';
 import Position from '../src/Position';
 import PositionTestHelper from './fixture/PositionTestHelper';
 
